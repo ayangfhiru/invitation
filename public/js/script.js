4d427735ee6d3027e642990871895046881af918
fhiru <fhiruayang@gmail.com>
2023-05-19 22:57:24 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+// Countdown
+var countdownDate = new Date("Dec 29 2023 09:00:00").getTime();
+var interval = setInterval(() => {
+    var now =  new Date().getTime();
+    var distance = countdownDate - now;
+
+    // Time calculation day, hours, minutes, seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // var result = days + " " + hours + " " + minutes + " " + seconds;
+
+    // Output
+    if(distance < 0) {
+        clearInterval(interval);
+        document.querySelector("#days").innerHTML = 0;
+        document.querySelector("#hours").innerHTML = 0;
+        document.querySelector("#minutes").innerHTML = 0;
+        document.querySelector("#seconds").innerHTML = 0;
+    } else {
+        document.querySelector("#days").innerHTML = days;
+        document.querySelector("#hours").innerHTML = hours;
+        document.querySelector("#minutes").innerHTML = minutes;
+        document.querySelector("#seconds").innerHTML = seconds;
+        // document.querySelector("#countdown").innerHTML = result;
+    }
+}, 1000);
+
+// Url to invitation
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const to = urlParams.get('to');
+
+if(queryString != ""){
+    if(to != ""){
+        document.querySelector("#invitation").innerHTML = to;
+    } else {
+        document.querySelector("#invitation").innerHTML = "Teman-teman Semua";
+    }
+} else {
+    document.querySelector("#invitation").innerHTML = "Teman-teman Semua";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f472b6',
+        'secondary': '#f9a8d4',
+        'txt': '#4c0519',
+      }
+    },
+  },
+  plugins: [],
+}
+
